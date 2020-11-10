23d4eb4ebaf059b52a5c6356d7a311dcee312010
Samuel Wood <samuel.wood@macys.com>
2020-11-10 10:42:39 -0500

    Simple Author and Book models added to a model package.

---
+++ src/main/java/com/example/SpringApp/model/Book.java
@@ -0,0 +1,42 @@
+package com.example.SpringApp.model;
+
+import java.util.List;
+
+public class Book {
+    private String title;
+    private String isbn;
+    private List<Author> authors;
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, List<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
+}
